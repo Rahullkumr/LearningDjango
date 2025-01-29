c1e5c1ac341b47b00c0286c2974d559b662783b6
Rahul Kumar <kumarrahul771996@gmail.com>
2025-01-29 22:47:15 +0530

    add: day11 notes

--- classProjects/ecom(mainfolder)/myproject/app1/views.py
+++ classProjects/ecom(mainfolder)/myproject/app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    print('hello world')
+    return 'homepage'
+
+
+# print(home())
---
+++ notes/day11.md
@@ -0,0 +1,3 @@
+# Day11: 29-Jan-2025
+
+- added a home route and displayed 'hello world' on console using views and urls files
