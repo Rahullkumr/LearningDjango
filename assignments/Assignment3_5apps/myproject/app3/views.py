78c92ccb9d5649f5e30065138b1894b5f3ffad42
Rahul Kumar <kumarrahul771996@gmail.com>
2025-02-03 00:09:56 +0530

    add: assignment03 create 5 apps with 5 views each

--- assignments/Assignment3_5apps/myproject/app3/views.py
+++ assignments/Assignment3_5apps/myproject/app3/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def fun1(request):
+    return HttpResponse('This is function 1/5 from app3')
+
+
+def fun2(request):
+    return HttpResponse('This is function 2/5 from app3')
+
+
+def fun3(request):
+    return HttpResponse('This is function 3/5 from app3')
+
+
+def fun4(request):
+    return HttpResponse('This is function 4/5 from app3')
+
+
+def fun5(request):
+    return HttpResponse('This is function 5/5 from app3')
