cdb52a02d6e2c5ece86462125a0cc04b3650043a
Rahul Kumar <kumarrahul771996@gmail.com>
2025-02-05 20:51:54 +0530

    add: task given during mock1

---
+++ practice/mock_task/myproject/app7/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from app7 import views
+
+urlpatterns = [
+    path('views1/', views.v1),
+    path('views2/', views.v2),
+    path('views3/', views.v3),
+    path('views4/', views.v4),
+    path('views5/', views.v5),
+    path('views6/', views.v6),
+    path('views7/', views.v7),
+    path('views8/', views.v8),
+    path('views9/', views.v9),
+    path('views10/', views.v10),
+]
---
+++ practice/mock_task/myproject/app7/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def v1(request):
+    return HttpResponse('This is views 1 from app7')
+
+
+def v2(request):
+    return HttpResponse('This is views 2 from app7')
+
+
+def v3(request):
+    return HttpResponse('This is views 3 from app7')
+
+
+def v4(request):
+    return HttpResponse('This is views 4 from app7')
+
+
+def v5(request):
+    return HttpResponse('This is views 5 from app7')
+
+
+def v6(request):
+    return HttpResponse('This is views 6 from app7')
+
+
+def v7(request):
+    return HttpResponse('This is views 7 from app7')
+
+
+def v8(request):
+    return HttpResponse('This is views 8 from app7')
+
+
+def v9(request):
+    return HttpResponse('This is views 9 from app7')
+
+
+def v10(request):
+    return HttpResponse('This is views 10 from app7')
