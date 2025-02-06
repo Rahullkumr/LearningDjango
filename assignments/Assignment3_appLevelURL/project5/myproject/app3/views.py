ba63a02dede86215537a1159296cc30f517ac1a2
Rahul Kumar <kumarrahul771996@gmail.com>
2025-02-06 11:52:34 +0530

    add:  assignment03's project5 with 10 apps with app level url

---
+++ assignments/Assignment3_appLevelURL/project5/myproject/app3/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def AndhraPradesh(request):
+    return HttpResponse('<h1>AndhraPradesh page</h1>')
+
+
+def Bihar(request):
+    return HttpResponse('<h1>Bihar page</h1>')
+
+
+def Chandigarh(request):
+    return HttpResponse('<h1>Chandigarh page</h1>')
+
+
+def Delhi(request):
+    return HttpResponse('<h1>Delhi page</h1>')
+
+
+def Haryana(request):
+    return HttpResponse('<h1>Haryana page</h1>')
+
+
+def Jharkhand(request):
+    return HttpResponse('<h1>Jharkhand page</h1>')
+
+
+def Maharashtra(request):
+    return HttpResponse('<h1>Maharashtra page</h1>')
+
+
+def Orissa(request):
+    return HttpResponse('<h1>Orissa page</h1>')
+
+
+def Telangana(request):
+    return HttpResponse('<h1>Telangana page</h1>')
+
+
+def Mizoram(request):
+    return HttpResponse('<h1>Mizoram page</h1>')
---
+++ assignments/Assignment3_appLevelURL/project5/myproject/app4/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from app4 import views
+
+urlpatterns = [
+    path('andhrapradesh', views.AndhraPradesh),
+    path('bihar', views.Bihar),
+    path('chandigarh', views.Chandigarh),
+    path('delhi', views.Delhi),
+    path('haryana', views.Haryana),
+    path('jharkhand', views.Jharkhand),
+    path('maharashtra', views.Maharashtra),
+    path('orissa', views.Orissa),
+    path('telangana', views.Telangana),
+    path('mizoram', views.Mizoram),
+]
