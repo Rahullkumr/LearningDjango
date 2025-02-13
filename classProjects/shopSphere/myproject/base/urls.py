721c4783271e563668c45887c2deb41a5486bd44
Rahul Kumar <kumarrahul771996@gmail.com>
2025-02-13 20:04:13 +0530

    update: base app of ShopSphere done

--- classProjects/shopSphere/myproject/base/urls.py
+++ classProjects/shopSphere/myproject/base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from base import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('about', views.about),
 ]
--- classProjects/shopSphere/myproject/base/views.py
+++ classProjects/shopSphere/myproject/base/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'temp_base/home.html')
+
+
+def about(request):
+    return render(request, 'temp_base/about.html')
