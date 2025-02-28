ef343bc68c58a8117e65fbf79af531ee8fa58df2
Rahul Kumar <kumarrahul771996@gmail.com>
2025-02-28 22:10:36 +0530

    update: IndianDefence project

---
+++ with_reactjs/IndianDefence/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+let Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="logo">Logo here</div>
+        <div className="links">
+          <a href="">Air</a>
+          <a href="">Water</a>
+          <a href="">Land</a>
+        </div>
+      </nav>
+    </>
+  );
+}
+export default Navbar
