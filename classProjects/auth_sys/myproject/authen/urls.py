00a8d27c14615339e05a5fbe64dd44731c114f3e
Rahul Kumar <kumarrahul771996@gmail.com>
2025-04-09 19:18:31 +0530

    feat: authorization

--- classProjects/auth_sys/myproject/authen/urls.py
+++ classProjects/auth_sys/myproject/authen/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logoutpage/', views.logoutpage, name='logoutpage'),
     path('profile/', views.profile, name='profile'),
     path('changepass/', views.changepass, name='changepass'),
+    path('updateprofile/<int:pk>', views.updateprofile, name='updateprofile'),
 ]
