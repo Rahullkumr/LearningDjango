6277119f79b4ebdaf5e016aeea0302a96423cc93
Rahul Kumar <kumarrahul771996@gmail.com>
2025-04-26 23:42:38 +0530

    rearrrange them once figured out

--- assignments/Assignment07_4projects/HospitalMgmtSys/hms/base/views.py
+++ assignments/Assignment07_4projects/HospitalMgmtSys/hms/base/views.py
@@ -5,8 +5,8 @@ from .models import Patient
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
 
 
 def registration(request):
---
+++ classProjects/relationship/myproject/foreign_key/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ForeignKeyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'foreign_key'
